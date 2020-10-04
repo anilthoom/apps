b48c540f3d3ad47302bca37e661f6dd970b43895
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-10-04 16:20:47 +0530

    Product Repository DAO

--- AppKart/SpringBootEcomm/src/main/java/com/anilt/ecomm/dao/ProductRepository.java
+++ AppKart/SpringBootEcomm/src/main/java/com/anilt/ecomm/dao/ProductRepository.java
@@ -1,4 +1,7 @@
 package com.anilt.ecomm.dao;
 
-public interface ProductRepository {
+import com.anilt.ecomm.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
