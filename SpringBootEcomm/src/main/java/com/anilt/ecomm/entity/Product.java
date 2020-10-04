4496a50f0dbb25fbb50175eb7812272f3ce11982
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-10-04 15:20:33 +0530

    Product entity

--- SpringBootEcomm/src/main/java/com/anilt/ecomm/entity/Product.java
+++ SpringBootEcomm/src/main/java/com/anilt/ecomm/entity/Product.java
@@ -4,9 +4,29 @@ import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import java.math.BigDecimal;
+import java.util.Date;
 
 @Entity
 @Table(name="product")
 @Data
 public class Product {
+
+    private Long id;
+
+    private String sku;
+
+    private String name;
+
+    private String description;
+
+    private BigDecimal unitPrice;
+
+    private String imageUrl;
+
+    private boolean active;
+
+    private Date dateCreated;
+
+    private Date lastUpdated;
 }
