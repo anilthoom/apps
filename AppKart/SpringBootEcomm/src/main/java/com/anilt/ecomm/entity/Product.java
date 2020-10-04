923d88f55d92b8cd1311213ff25030f397ef0dd7
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-10-04 15:57:40 +0530

    Product Entity

--- AppKart/SpringBootEcomm/src/main/java/com/anilt/ecomm/entity/Product.java
+++ AppKart/SpringBootEcomm/src/main/java/com/anilt/ecomm/entity/Product.java
@@ -2,8 +2,7 @@ package com.anilt.ecomm.entity;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.math.BigDecimal;
 import java.util.Date;
 
@@ -12,23 +11,35 @@ import java.util.Date;
 @Data
 public class Product {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
+    @Column(name = "sku")
     private String sku;
 
+    @Column(name = "name")
     private String name;
 
+    @Column(name = "description")
     private String description;
 
+    @Column(name = "unit_price")
     private BigDecimal unitPrice;
 
+    @Column(name = "image_url")
     private String imageUrl;
 
+    @Column(name = "active")
     private boolean active;
 
+    @Column(name = "units_in_stock")
     private int unitsInStock;
 
+    @Column(name = "date_created")
     private Date dateCreated;
 
+    @Column(name = "last_updated")
     private Date lastUpdated;
 }
