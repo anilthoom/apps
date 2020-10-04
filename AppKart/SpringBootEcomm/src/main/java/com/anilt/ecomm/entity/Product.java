1011dee7590f005ebb1dcb3215c79151021acc4d
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-10-04 15:59:48 +0530

    Product Entity

--- AppKart/SpringBootEcomm/src/main/java/com/anilt/ecomm/entity/Product.java
+++ AppKart/SpringBootEcomm/src/main/java/com/anilt/ecomm/entity/Product.java
@@ -1,6 +1,8 @@
 package com.anilt.ecomm.entity;
 
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
@@ -38,8 +40,10 @@ public class Product {
     private int unitsInStock;
 
     @Column(name = "date_created")
+    @CreationTimestamp
     private Date dateCreated;
 
     @Column(name = "last_updated")
+    @UpdateTimestamp
     private Date lastUpdated;
 }
