44cb83139418c7fbdc2aa9a30c8581935511dc53
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-10-04 15:15:33 +0530

    Product entity

--- SpringBootEcomm/src/main/java/com/anilt/ecomm/entity/Product.java
+++ SpringBootEcomm/src/main/java/com/anilt/ecomm/entity/Product.java
@@ -1,4 +1,12 @@
 package com.anilt.ecomm.entity;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="product")
+@Data
 public class Product {
 }
