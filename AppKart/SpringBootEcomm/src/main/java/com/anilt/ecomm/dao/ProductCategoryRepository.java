29b1afb72661b1d4da6e9b02ffa7a6b7893feee5
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-10-04 17:00:11 +0530

    ProductCategoryRepository DAO

--- AppKart/SpringBootEcomm/src/main/java/com/anilt/ecomm/dao/ProductCategoryRepository.java
+++ AppKart/SpringBootEcomm/src/main/java/com/anilt/ecomm/dao/ProductCategoryRepository.java
@@ -2,6 +2,8 @@ package com.anilt.ecomm.dao;
 
 import com.anilt.ecomm.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
