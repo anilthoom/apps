5493cf00ff39a68f9bf100ca7ce6ab5a62e4dc3f
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-10-04 17:40:39 +0530

    Configuration

---
+++ AppKart/SpringBootEcomm/src/main/java/com/anilt/ecomm/config/MyDataRestConfig.java
@@ -0,0 +1,28 @@
+package com.anilt.ecomm.config;
+
+import com.anilt.ecomm.entity.Product;
+import com.anilt.ecomm.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        HttpMethod[] theUnsupportedActions = {HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE};
+
+        //Disable Product POST PUT and DELETE
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+        //Disable ProductCategory POST PUT and DELETE
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+    }
+}
