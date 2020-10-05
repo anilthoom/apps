583bd8337d55ae651428ce50ef37d47c32ca3cd3
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-10-05 12:39:25 +0530

    Cross origin is allowed

--- AppKart/SpringBootEcomm/src/main/java/com/anilt/ecomm/dao/ProductRepository.java
+++ AppKart/SpringBootEcomm/src/main/java/com/anilt/ecomm/dao/ProductRepository.java
@@ -2,6 +2,8 @@ package com.anilt.ecomm.dao;
 
 import com.anilt.ecomm.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
