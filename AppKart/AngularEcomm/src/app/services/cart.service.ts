d6ff09088bfb535f05379970f39894b62104aa3f
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-10-11 20:07:48 +0530

    remove & decrement functionality

--- AppKart/AngularEcomm/src/app/services/cart.service.ts
+++ AppKart/AngularEcomm/src/app/services/cart.service.ts
@@ -7,8 +7,9 @@ import { CartItem } from '../common/cart-item';
 })
 export class CartService {
 
+
   cartItems: CartItem[] = [];
-  
+
   /**
    * Subject is a subclass of Observable. 
    * We can use subject to publish evernts
@@ -19,11 +20,11 @@ export class CartService {
 
   constructor() { }
 
-  addToCart(theCartItem: CartItem){
+  addToCart(theCartItem: CartItem) {
     let alreadyExistsInCart: boolean = false;
     let existingCartItem: CartItem = undefined;
 
-    if(this.cartItems.length >0){
+    if (this.cartItems.length > 0) {
       //Checking if item already exists in the cart.
       /*for(let tempCartItem of this.cartItems){
         if(tempCartItem.id === theCartItem.id){
@@ -32,28 +33,46 @@ export class CartService {
         }
       }*/
       //Optimized code.
-      existingCartItem = this.cartItems.find( tempCartItem => tempCartItem.id === theCartItem.id);
+      existingCartItem = this.cartItems.find(tempCartItem => tempCartItem.id === theCartItem.id);
 
       alreadyExistsInCart = (existingCartItem != undefined);
     }
 
-    if(alreadyExistsInCart){
+    if (alreadyExistsInCart) {
       //so we need to increment
       existingCartItem.quantity++;
     }
-    else{
+    else {
       this.cartItems.push(theCartItem);
     }
 
     //Compute total price and quantity
     this.computeCartTotals();
-    
+
+  }
+  decrementQuantity(theCartItem: CartItem) {
+    theCartItem.quantity--;
+    if(theCartItem.quantity == 0){
+      this.remove(theCartItem);
+    }
+    else{
+      this.computeCartTotals();
+    }
+  }
+  remove(theCartItem: CartItem){
+    const itemIndex = this.cartItems.findIndex(tempCartItem => tempCartItem.id === theCartItem.id);
+
+    if(itemIndex > -1){
+      this.cartItems.splice(itemIndex, 1);
+
+      this.computeCartTotals();
+    }
   }
   computeCartTotals() {
     let totalPriceValue: number = 0;
     let totalQuantityValue: number = 0;
 
-    for(let currentCarItem of this.cartItems){
+    for (let currentCarItem of this.cartItems) {
       totalPriceValue += currentCarItem.quantity * currentCarItem.unitPrice;
       totalQuantityValue += currentCarItem.quantity;
     }
