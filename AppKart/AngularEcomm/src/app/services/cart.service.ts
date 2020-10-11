ce3d9886020474e210c5aaf98db3bc8b80fc3ba0
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-10-11 17:57:42 +0530

    addToCart service

--- AppKart/AngularEcomm/src/app/services/cart.service.ts
+++ AppKart/AngularEcomm/src/app/services/cart.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { CartItem } from '../common/cart-item';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
 
+  cartItems: CartItem[] = [];
+  
+  /**
+   * Subject is a subclass of Observable. 
+   * We can use subject to publish evernts
+   * This event will be sent to all of the subscribers
+   */
+  totalPrice: Subject<number> = new Subject<number>();
+  totalQuantity: Subject<number> = new Subject<number>();
+
   constructor() { }
+
+  addToCart(theCartItem: CartItem){
+    
+  }
 }
