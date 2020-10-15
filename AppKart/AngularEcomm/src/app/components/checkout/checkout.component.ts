f8b217ab3cbf2f719ddb1b0eff1ab647daf6eae3
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-10-15 21:18:41 +0530

    validators

--- AppKart/AngularEcomm/src/app/components/checkout/checkout.component.ts
+++ AppKart/AngularEcomm/src/app/components/checkout/checkout.component.ts
@@ -52,10 +52,10 @@ export class CheckoutComponent implements OnInit {
         zipCode: new FormControl('', [Validators.required, Validators.minLength(2), CustomValidator.nonOnlyWhitespace])
       }),
       creditCard: this.formBuilder.group({
-        cardType:[''],
-        nameOnCard:[''],
-        cardNumber:[''],
-        securityCode:[''],
+        cardType: new FormControl('', [Validators.required]),
+        nameOnCard: new FormControl('', [Validators.required, Validators.minLength(2), CustomValidator.nonOnlyWhitespace]),
+        cardNumber: new FormControl('', [Validators.required, Validators.pattern('[0-9]{16}')]),
+        securityCode: new FormControl('', [Validators.required, Validators.pattern('[0-9]{3}')]),
         expirationMonth:[''],
         expirationYear:['']
       })
@@ -106,6 +106,12 @@ export class CheckoutComponent implements OnInit {
   get billingAddressState(){return this.checkoutFormGroup.get('billingAddress.state');}
   get billingAddressCountry(){return this.checkoutFormGroup.get('billingAddress.country');}
   get billingAddressZipCode(){return this.checkoutFormGroup.get('billingAddress.zipCode');}
+
+  get creditCardType(){return this.checkoutFormGroup.get('creditCard.cardType');}
+  get creditCardNameOnCard(){return this.checkoutFormGroup.get('creditCard.nameOnCard');}
+  get creditCardNumber(){return this.checkoutFormGroup.get('creditCard.cardNumber');}
+  get creditCardSecurityCode(){return this.checkoutFormGroup.get('creditCard.securityCode');}
+
 
   copyShippingAddToBillingAdd(event){
     if(event.target.checked){
