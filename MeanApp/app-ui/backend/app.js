18589c816965450a9fb59d23a198941804e898d5
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-10-20 13:13:50 +0530

    new REST API in nodeJS

--- MeanApp/app-ui/backend/app.js
+++ MeanApp/app-ui/backend/app.js
@@ -1,13 +1,24 @@
 const express = require('express');
 
 const app = express();
-app.use((req, res, next)=> {
-    console.log('First')
-    next();
-});
 
-app.use((req, res, next)=> {
-    res.send('Hello from express!!')
+app.use('/api/posts', (req, res, next)=> {
+    const posts = [
+        { 
+            id: 'fdsfs2093', 
+            title: 'FIRST SERVER SIDE', 
+            content: 'This is coming from server'
+        },
+        { 
+            id: 'xxew23344', 
+            title: 'SECOND SERVER SIDE', 
+            content: 'This is ALSO coming from server'
+        }
+    ];
+    res.status(200).json({
+        message: 'Posts Fetched Successfully!',
+        posts: posts
+    });
 });
 
 module.exports = app;
