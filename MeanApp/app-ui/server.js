5c9c1e5e858d52f0b25f427163bbb0a50e0b43c6
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-10-20 11:15:42 +0530

    server js

--- MeanApp/app-ui/server.js
+++ MeanApp/app-ui/server.js
@@ -1,6 +1,8 @@
 const http = require('http');
-const server = http.createServer((req, res) =>{
-    res.end('Server Response')
-});
+const app = require('./backend/app');
 
-server.listen(process.env.PORT || 3000)
+const port = process.env.PORT || 3000;
+
+app.set('port', port)
+const server = http.createServer(app);
+server.listen(port);
