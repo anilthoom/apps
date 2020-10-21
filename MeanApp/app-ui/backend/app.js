faedf757cbb6cf869cb0251a7461537516de8f46
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-10-21 12:01:51 +0530

    Fetched from database

--- MeanApp/app-ui/backend/app.js
+++ MeanApp/app-ui/backend/app.js
@@ -43,22 +43,13 @@ app.post("/api/posts", (req, res, next) => {
 });
 
 app.get('/api/posts', (req, res, next)=> {
-    const posts = [
-        { 
-            id: 'fdsfs2093', 
-            title: 'FIRST SERVER SIDE', 
-            content: 'This is coming from server'
-        },
-        { 
-            id: 'xxew23344', 
-            title: 'SECOND SERVER SIDE', 
-            content: 'This is ALSO coming from server'
-        }
-    ];
-    res.status(200).json({
-        message: 'Posts Fetched Successfully!',
-        posts: posts
-    });
+    
+    Post.find().then(documents => {
+        res.status(200).json({
+            message: 'Posts Fetched Successfully!',
+            posts: documents
+        });
+    });  
 });
 
 module.exports = app;
