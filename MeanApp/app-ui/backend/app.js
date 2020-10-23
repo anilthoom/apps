7d906636ec501648848c5676ba7d0b917957643a
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-10-23 09:37:33 +0530

    Format changes

--- MeanApp/app-ui/backend/app.js
+++ MeanApp/app-ui/backend/app.js
@@ -15,14 +15,14 @@ mongoose.connect("mongodb+srv://anilt:MTkBiEytM4tj21YI@cluster0.hvudy.mongodb.ne
     });
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use((req, res, next)=>{
+app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader(
-        'Access-Control-Allow-Headers', 
+        'Access-Control-Allow-Headers',
         'Origin, X-Requested-With, Content-Type, Accept'
-        );
+    );
     res.setHeader(
         'Access-Control-Allow-Methods',
         'GET, POST, PATCH, DELETE, OPTIONS'
@@ -42,14 +42,14 @@ app.post("/api/posts", (req, res, next) => {
     });
 });
 
-app.get('/api/posts', (req, res, next)=> {
-    
+app.get('/api/posts', (req, res, next) => {
+
     Post.find().then(documents => {
         res.status(200).json({
             message: 'Posts fetched successfully!',
             posts: documents
         });
-    });  
+    });
 });
 
 module.exports = app;
