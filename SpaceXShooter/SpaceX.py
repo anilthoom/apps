b409280b15380c2d016b35865b30f968be5f3966
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-10-26 18:01:04 +0530

    Changes

---
+++ SpaceXShooter/SpaceX.py
@@ -0,0 +1,21 @@
+import pygame
+
+# Intialize the pygame
+pygame.init()
+
+# create the screen
+screen = pygame.display.set_mode((800, 600))
+
+
+
+
+# Game Loop
+running = True
+while running:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((0, 0, 0))
+    pygame.display.update()
