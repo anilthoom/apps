792b12e6e6f40d2f1fc956dca1308d4fe345258c
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-10-26 18:31:28 +0530

    Added : cordinates for the player

--- SpaceXShooter/SpaceX.py
+++ SpaceXShooter/SpaceX.py
@@ -39,3 +39,13 @@ while running:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 playerX_change = 0
 
+    playerX += playerX_change
+
+    if playerX <= 0:
+        playerX = 0
+    elif playerX >= 736:
+        playerX = 736
+
+    player(playerX, playerY)
+    pygame.display.update()
+
