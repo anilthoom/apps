51c61e5fad87cd3780fcef644eb19c8e6b365695
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-10-26 18:20:45 +0530

    Added : Caption and Icon

--- SpaceXShooter/SpaceX.py
+++ SpaceXShooter/SpaceX.py
@@ -5,3 +5,8 @@ pygame.init()
 # create the screen
 screen = pygame.display.set_mode((800, 600))
 
+# Caption and Icon
+pygame.display.set_caption("Space Invader")
+icon = pygame.image.load('ufo.png')
+pygame.display.set_icon(icon)
+
