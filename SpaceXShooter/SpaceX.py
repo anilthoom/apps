5daae0d52a0034a60d159d6d894dd2d6f1120089
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-10-26 18:47:41 +0530

    Added : baground and Enimy

--- SpaceXShooter/SpaceX.py
+++ SpaceXShooter/SpaceX.py
@@ -1,9 +1,16 @@
+import math
+import random
+
 import pygame
 
 # Intialize the pygame
 pygame.init()
+
 # create the screen
 screen = pygame.display.set_mode((800, 600))
+
+# Background
+background = pygame.image.load('background.png')
 
 # Caption and Icon
 pygame.display.set_caption("Space Invader")
@@ -16,36 +23,68 @@ playerX = 370
 playerY = 480
 playerX_change = 0
 
+# Enemy
+enemyImg = []
+enemyX = []
+enemyY = []
+enemyX_change = []
+enemyY_change = []
+num_of_enemies = 6
+
+for i in range(num_of_enemies):
+    enemyImg.append(pygame.image.load('enemy.png'))
+
 
 def player(x, y):
     screen.blit(playerImg, (x, y))
 
 
+def enemy(x, y, i):
+    screen.blit(enemyImg[i], (x, y))
+
+
+# Game Loop
 running = True
 while running:
 
+    # RGB = Red, Green, Blue
     screen.fill((0, 0, 0))
-
+    # Background Image
+    screen.blit(background, (0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+        # if keystroke is pressed check whether its right or left
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                playerX_change = -0.3
+                playerX_change = -5
             if event.key == pygame.K_RIGHT:
-                playerX_change = 0.3
+                playerX_change = 5
+
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 playerX_change = 0
 
-    playerX += playerX_change
+    # 5 = 5 + -0.1 -> 5 = 5 - 0.1
+    # 5 = 5 + 0.1
 
+    playerX += playerX_change
     if playerX <= 0:
         playerX = 0
     elif playerX >= 736:
         playerX = 736
 
-    player(playerX, playerY)
-    pygame.display.update()
+    # Enemy Movement
+    for i in range(num_of_enemies):
+        enemyX += enemyX_change
+        if enemyX <= 0:
+            enemyX_change = 4
+            enemyY += enemyY_change
+        elif enemyX >= 736:
+            enemyX_change = -4
+            enemyY += enemyY_change
 
+        #
+
+        enemy(enemyX, enemyY,i)
