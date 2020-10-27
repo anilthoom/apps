9092ee4f0171aa1a5f1e7aaab3d8c87aa1a96b5b
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-10-27 19:12:49 +0530

    Styles

--- SpaceXShooter/SpaceX.py
+++ SpaceXShooter/SpaceX.py
@@ -7,10 +7,22 @@ screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("SpaceX Fight!!")
 pygame.display.set_icon(pygame.image.load('ufo.png'))
 
+playerImg = pygame.image.load('player.png')
+playerX = 370
+playerY = 480
+
+def player():
+    screen.blit(playerImg, (playerX, playerY))
+
 # Game loop
 running = True
 while running:
+    # RGB color values
+    screen.fill((0, 0, 0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    screen.fill((0, 0, 0))
+
+    player()
+    pygame.display.update()
