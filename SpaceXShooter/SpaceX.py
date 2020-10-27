483535c5c5dd36e2e9c996835c39f78391aeec99
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-10-27 10:29:06 +0530

    code removal

--- SpaceXShooter/SpaceX.py
+++ SpaceXShooter/SpaceX.py
@@ -41,27 +41,3 @@ def player(x, y):
 
 def enemy(x, y, i):
     screen.blit(enemyImg[i], (x, y))
-
-
-# Game Loop
-running = True
-while running:
-
-    # RGB = Red, Green, Blue
-    screen.fill((0, 0, 0))
-    # Background Image
-    screen.blit(background, (0, 0))
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-        # if keystroke is pressed check whether its right or left
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                playerX_change = -5
-            if event.key == pygame.K_RIGHT:
-                playerX_change = 5
-
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                playerX_change = 0
