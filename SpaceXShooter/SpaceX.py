5e5483c1cf86772880ed1fce6b01672aee1b8340
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-10-27 20:01:40 +0530

    key strokes

--- SpaceXShooter/SpaceX.py
+++ SpaceXShooter/SpaceX.py
@@ -10,6 +10,7 @@ pygame.display.set_icon(pygame.image.load('ufo.png'))
 playerImg = pygame.image.load('player.png')
 playerX = 370
 playerY = 480
+playerX_change = 0
 
 
 def player(x, y):
@@ -30,10 +31,19 @@ while running:
         # key strokes logic
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                playerX -= 0.1
-            if event.key == pygame.K_LEFT:
-                playerX += 0.1
+                playerX_change = -0.1
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 0.1
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
 
+    playerX += playerX_change
+
+    if playerX <= 0:
+        playerX = 0
+    elif playerX >= 736:
+        playerX = 736
 
     player(playerX, playerY)
     pygame.display.update()
