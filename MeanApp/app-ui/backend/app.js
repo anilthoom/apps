ec8f432263a72dad425581bc468664b56751f51c
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-10-28 18:32:04 +0530

    bug fix

--- MeanApp/app-ui/backend/app.js
+++ MeanApp/app-ui/backend/app.js
@@ -52,7 +52,7 @@ app.get('/api/posts', (req, res, next) => {
     });
 });
 
-app.get('/api/posts/:id', (req, res, next) => {
+app.delete('/api/posts/:id', (req, res, next) => {
     console.log(req.params.id);
     res.status(200).json({ message: "Post deleted!"});
 });
