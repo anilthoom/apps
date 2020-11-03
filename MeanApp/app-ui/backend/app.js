58116c7395ac808ebb62d0f404de84d8964123fa
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-11-03 11:09:59 +0530

    Format change

--- MeanApp/app-ui/backend/app.js
+++ MeanApp/app-ui/backend/app.js
@@ -39,7 +39,7 @@ app.post("/api/posts", (req, res, next) => {
         res.status(201).json({
             message: 'Post added successfully!!',
             postId: createdPost._id
-        });    
+        });
     });
 });
 
@@ -54,9 +54,9 @@ app.get('/api/posts', (req, res, next) => {
 });
 
 app.delete('/api/posts/:id', (req, res, next) => {
-    Post.deleteOne({_id: req.params.id}).then(result => {
+    Post.deleteOne({ _id: req.params.id }).then(result => {
         console.log(result);
-        res.status(200).json({ message: "Post deleted!"});
+        res.status(200).json({ message: "Post deleted!" });
     });
 });
 
