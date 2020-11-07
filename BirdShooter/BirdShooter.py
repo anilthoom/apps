e24655014f7d085c685a26b20b2f13f7a2fc7d78
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-11-07 18:55:04 +0530

    bird is moving

--- BirdShooter/BirdShooter.py
+++ BirdShooter/BirdShooter.py
@@ -6,44 +6,31 @@ pygame.init()
 screen = pygame.display.set_mode((800, 600))
 backgroundImage = pygame.image.load("images/background.jpg")
 
-# Enemy Image
-doveR2LImage = []
-doveR2LImageX = []
-doveR2LImageY = []
-doveR2LImageX_change = []
-doveR2LImageY_change = []
-num_of_enemies = 1
-
-for i in range(num_of_enemies):
-    doveR2LImage.append(pygame.image.load('images/dove32R2L.png'))
-    doveR2LImageX.append(random.randint(0, 735))
-    doveR2LImageY.append(random.randint(50, 150))
-    doveR2LImageX_change.append(2)
-    doveR2LImageY_change.append(40)
-
 # Dove right to left image
-def doveR2LTravel(x, y, i):
-    screen.blit(doveR2LImage[i], (x, y))
+doveR2LImage = pygame.image.load("images/dove32R2L.png")
+doveR2LImageX = 780
+doveR2LImageY = 480
+doveR2LImageX_change = 0.3
+doveR2LImageY_change = 0
+
+
+def doveR2LTravel(x, y):
+    screen.blit(doveR2LImage, (x, y))
 
 
 running = True
 while running:
     screen.blit(backgroundImage, (0, 0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        # Enemy movement
-    for i in range(num_of_enemies):
-        doveR2LImageX[i] += doveR2LImageX_change[i]
-        if doveR2LImageX[i] <= 0:
-            doveR2LImageX_change[i] = 0.01
-            doveR2LImageY[i] += doveR2LImageY_change[i]
-        elif doveR2LImageX[i] >= 736:
-            doveR2LImageX_change[i] = -0.01
-            doveR2LImageY[i] += doveR2LImageY_change[i]
 
-        doveR2LImageX[i] = random.randint(0, 735)
-        doveR2LImageY[i] = random.randint(50, 150)
-        doveR2LTravel(doveR2LImageX[i], doveR2LImageY[i], i)
+    if doveR2LImageX <= 0:
+        doveR2LImageX = 780
+    doveR2LImageX -= doveR2LImageX_change
+
+    doveR2LTravel(doveR2LImageX, 180)
+
     # To display the background image and screen update
     pygame.display.update()
