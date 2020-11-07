2ae7a17bbd8b6efcbcd8dcdc0d1a50df8ff4cc4d
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-11-07 19:04:56 +0530

    birds are moving

--- BirdShooter/BirdShooter.py
+++ BirdShooter/BirdShooter.py
@@ -13,9 +13,15 @@ doveR2LImageY = 480
 doveR2LImageX_change = 0.3
 doveR2LImageY_change = 0
 
+# Dove right to left image
+doveL2RImage = pygame.image.load("images/dove32L2R.png")
+doveL2RImageX = 0
+doveL2RImageY = 450
+doveL2RImageX_change = 0.2
+doveL2RImageY_change = 0
 
-def doveR2LTravel(x, y):
-    screen.blit(doveR2LImage, (x, y))
+def doveTravel(x, y, bird):
+    screen.blit(bird, (x, y))
 
 
 running = True
@@ -29,8 +35,12 @@ while running:
     if doveR2LImageX <= 0:
         doveR2LImageX = 780
     doveR2LImageX -= doveR2LImageX_change
+    doveTravel(doveR2LImageX, 180, doveR2LImage)
 
-    doveR2LTravel(doveR2LImageX, 180)
+    if doveL2RImageX >= 780:
+        doveL2RImageX = 0
+    doveL2RImageX += doveL2RImageX_change
+    doveTravel(doveL2RImageX, 210, doveL2RImage)
 
     # To display the background image and screen update
     pygame.display.update()
