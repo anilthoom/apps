837c7f373f74280b5216cf8fc0b6b083f30a98d0
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-12-04 19:47:25 +0530

    Code change

--- BirdShooter/BirdShooter.py
+++ BirdShooter/BirdShooter.py
@@ -18,6 +18,12 @@ pygame.display.set_icon(icon)
 def flyBird(birdImage, direction, xAxis, yAxis):
     print("Yet... to fly...")
 
+# Dove right to left image
+doveR2LImage = pygame.image.load("images/dove32R2L.png")
+doveR2LImageX = 780
+doveR2LImageY = 180
+doveR2LImageX_change = 1
+doveR2LImageY_change = 1
 
 
 # Dove right to left image
