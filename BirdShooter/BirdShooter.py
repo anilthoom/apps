e7f9920dd6454e460d935754b6d2df44096119d9
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-12-15 14:39:52 +0530

    Refactoring

--- BirdShooter/BirdShooter.py
+++ BirdShooter/BirdShooter.py
@@ -108,8 +108,7 @@ while done:
 
     doveTravel(doveR2LImageX, doveR2LImageY, doveR2LImage)
 
-
-    #Need to relook
+    # Need to relook
     if doveL2RImageX >= 780:
         doveL2RImageX = 0
     if doveL2RImageY >= 600:
