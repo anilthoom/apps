c5196d25c5cc59b2a189cd2ea2281b1da3a5b9e8
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-12-19 08:45:51 +0530

    lol

--- BirdShooter/BirdShooter.py
+++ BirdShooter/BirdShooter.py
@@ -54,13 +54,6 @@ def displayTargetAtMouseCursor():
     pygame.mouse.set_visible(False)
 
 
-# Collision logic goes here...
-def isCollision(birdX, birdY, bulletX, bulletY):
-    distance = math.sqrt((math.pow(birdX - bulletX, 2)) + (math.pow(birdY - bulletY, 2)))
-    if distance < 17:
-        return True
-    else:
-        return False
 
 
 # Running the game window
