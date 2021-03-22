f1ae0271e301804701126b4854d12c60457985c4
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-03-22 13:02:02 +0530

    Component library application client

--- ContentLibMSWord/contentlib-client/src/app/component.service.ts
+++ ContentLibMSWord/contentlib-client/src/app/component.service.ts
@@ -4,6 +4,12 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ComponentService {
+  deleteComponentLib(id: number) {
+    throw new Error("Method not implemented.");
+  }
+  getComponentLibList(): import("rxjs").Observable<import("./componentlib").Componentlib[]> {
+    throw new Error("Method not implemented.");
+  }
 
   constructor() { }
 }
