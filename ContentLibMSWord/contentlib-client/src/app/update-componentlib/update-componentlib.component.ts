d69ff60191f27d6393d538027fcd03b349f469df
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-03-22 13:35:06 +0530

    update comp lib

--- ContentLibMSWord/contentlib-client/src/app/update-componentlib/update-componentlib.component.ts
+++ ContentLibMSWord/contentlib-client/src/app/update-componentlib/update-componentlib.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Componentlib } from '../componentlib';
+import { ComponentService } from '../component.service';
 
 @Component({
   selector: 'app-update-componentlib',
@@ -6,6 +9,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./update-componentlib.component.css']
 })
 export class UpdateComponentlibComponent implements OnInit {
+
+  id: number;
+  complib: Componentlib;
 
   constructor() { }
 
