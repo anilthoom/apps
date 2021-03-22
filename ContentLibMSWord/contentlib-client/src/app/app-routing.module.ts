49636d63012c1a524d9223c1980622c97d019c0d
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-03-22 14:36:20 +0530

    routing

--- ContentLibMSWord/contentlib-client/src/app/app-routing.module.ts
+++ ContentLibMSWord/contentlib-client/src/app/app-routing.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { ComponentlibDetailsComponent } from './componentlib-details/componentlib-details.component';
+import { CreateComponentlibComponent } from './create-componentlib/create-componentlib.component';
+import { ComponentlibListComponent } from './componentlib-list/componentlib-list.component';
+import { UpdateComponentlibComponent } from './update-componentlib/update-componentlib.component';
+
 const routes: Routes = [];
 
 @NgModule({
