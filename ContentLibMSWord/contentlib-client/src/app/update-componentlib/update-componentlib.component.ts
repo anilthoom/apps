c649039bdfcd5fb341e0484999064c7871f66d65
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-03-22 13:29:56 +0530

    update component lib

---
+++ ContentLibMSWord/contentlib-client/src/app/update-componentlib/update-componentlib.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-update-componentlib',
+  templateUrl: './update-componentlib.component.html',
+  styleUrls: ['./update-componentlib.component.css']
+})
+export class UpdateComponentlibComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
