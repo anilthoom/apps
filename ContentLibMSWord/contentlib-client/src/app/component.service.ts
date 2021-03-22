fd0e122e23fd9aec14b70162d186d9957ee0a611
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-03-22 13:22:41 +0530

    component service

--- ContentLibMSWord/contentlib-client/src/app/component.service.ts
+++ ContentLibMSWord/contentlib-client/src/app/component.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { Componentlib } from './componentlib';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ComponentService {
+  createComponentlib(complib: Componentlib) {
+    throw new Error("Method not implemented.");
+  }
   deleteComponentLib(id: number) {
     throw new Error("Method not implemented.");
   }
