4bf09bcd4693bf73cda6e51e2aea44f664d050d8
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-03-22 14:39:54 +0530

    app routing

--- ContentLibMSWord/contentlib-client/src/app/app-routing.module.ts
+++ ContentLibMSWord/contentlib-client/src/app/app-routing.module.ts
@@ -6,7 +6,13 @@ import { CreateComponentlibComponent } from './create-componentlib/create-compon
 import { ComponentlibListComponent } from './componentlib-list/componentlib-list.component';
 import { UpdateComponentlibComponent } from './update-componentlib/update-componentlib.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo:'components', pathMatch: 'full'},
+  { path: 'components', component: ComponentlibListComponent },
+  { path: 'add', component: CreateComponentlibComponent },
+  { path: 'update/:id', component: UpdateComponentlibComponent },
+  { path: 'details/:id', component: ComponentlibDetailsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
