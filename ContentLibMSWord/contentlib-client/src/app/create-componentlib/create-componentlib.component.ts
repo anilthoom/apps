ca7f23711d5b8eb7e3b32becdbc031473ff920cf
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-03-22 13:08:57 +0530

    Component library application client

--- ContentLibMSWord/contentlib-client/src/app/create-componentlib/create-componentlib.component.ts
+++ ContentLibMSWord/contentlib-client/src/app/create-componentlib/create-componentlib.component.ts
@@ -1,4 +1,7 @@
+import { ComponentService } from "../component.service";
+import { Componentlib } from "../componentlib";
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-componentlib',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateComponentlibComponent implements OnInit {
 
-  constructor() { }
+  componentlib: Componentlib = new Componentlib();
+  submitted = false;
+
+  constructor(private componentlibService: ComponentService,
+    private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  newComponentLib: void {
+    this.submitted = false;
+    this.componentlib = new Componentlib();
+  }
+
+  save() {
+    this.componentlibService
+    .createComponentlib(this.componentlib).subscribe(data =>{
+      console.log(data)
+      this.componentlib = new Componentlib();
+      this.gotoList();
+    },
+    error => console.log(error));
+  }
+
+  onsubmit() {
+    this.submitted = true;
+    this.save();
+  }
+  gotoList(){
+    this.router.navigate(['/components'])
+  }
 }
