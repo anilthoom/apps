d3b02c128dbb4dec56b26b4f17cd0352ae2264d6
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-04-06 10:42:32 +0530

    Get all components

--- ContentLibMSWord/componentlibrary/src/main/java/com/anilt/component/library/ComponentLibraryApplication.java
+++ ContentLibMSWord/componentlibrary/src/main/java/com/anilt/component/library/ComponentLibraryApplication.java
@@ -9,7 +9,7 @@ public class ComponentLibraryApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ComponentLibraryApplication.class, args);
 	}
-	// http://localhost:8080/cl/api/v1/components GET all
+	// http://localhost:8080/cl/api/v1/components GET all components
 	// http://localhost:8080/cl/api/v1/components/{componentId} GET
 	// http://localhost:8080/cl/api/v1/components Create { "contentHtml":"This is HTML3 content", "contentText":"This is TEXT3 content" }
 	// http://localhost:8080/cl/api/v1/components/{componentId} Update
