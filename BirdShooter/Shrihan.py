9523beb0cbd13e1ea064e4d2edd56f2e7b2d3824
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-04-11 19:38:13 +0530

    Commit Message

--- BirdShooter/Shrihan.py
+++ BirdShooter/Shrihan.py
@@ -15,11 +15,12 @@ TICKS_PER_SEC = 60
 # Size of sectors used to ease block loading.
 SECTOR_SIZE = 16
 
-WALKING_SPEED = 5
-FLYING_SPEED = 15
+WALKING_SPEED = 1
+FLYING_SPEED = 100
 
-GRAVITY = 20.0
-MAX_JUMP_HEIGHT = 1.0 # About the height of a block.
+GRAVITY = 0
+
+MAX_JUMP_HEIGHT = 1 # About the height of a block.
 # To derive the formula for calculating jump speed, first solve
 #    v_t = v_0 + a * t
 # for the time at which you achieve maximum height, where a is the acceleration
