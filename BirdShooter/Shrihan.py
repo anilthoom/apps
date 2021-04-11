504158d8da682752d711abbc70a47dfcb154faf1
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-04-11 20:07:30 +0530

    Commit Message

--- BirdShooter/Shrihan.py
+++ BirdShooter/Shrihan.py
@@ -1,5 +1,6 @@
 from __future__ import division
 import sys
+import pygame
 import math
 import random
 import time
@@ -10,17 +11,18 @@ from pyglet.gl import *
 from pyglet.graphics import TextureGroup
 from pyglet.window import key, mouse
 
+
 TICKS_PER_SEC = 60
 
 # Size of sectors used to ease block loading.
-SECTOR_SIZE = 16
+SECTOR_SIZE = 100
 
 WALKING_SPEED = 15
-FLYING_SPEED = 100
+FLYING_SPEED = 1
 
-GRAVITY = 15
+GRAVITY = 1
 
-MAX_JUMP_HEIGHT = 1 # About the height of a block.
+MAX_JUMP_HEIGHT = 5 # About the height of a block.
 # To derive the formula for calculating jump speed, first solve
 #    v_t = v_0 + a * t
 # for the time at which you achieve maximum height, where a is the acceleration
@@ -29,9 +31,9 @@ MAX_JUMP_HEIGHT = 1 # About the height of a block.
 # Use t and the desired MAX_JUMP_HEIGHT to solve for v_0 (jump speed) in
 #    s = s_0 + v_0 * t + (a * t^2) / 2
 JUMP_SPEED = math.sqrt(2 * GRAVITY * MAX_JUMP_HEIGHT)
-TERMINAL_VELOCITY = 50
+TERMINAL_VELOCITY = 15
 
-PLAYER_HEIGHT = 1
+PLAYER_HEIGHT = 2
 
 if sys.version_info[0] >= 3:
     xrange = range
@@ -305,7 +307,7 @@ class Model(object):
         texture = self.world[position]
         self.shown[position] = texture
         if immediate:
-            self._show_block(position, texture)
+            self._show_block(position,texture)
         else:
             self._enqueue(self._show_block, position, texture)
 
