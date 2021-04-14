13598f8a5773db604060786a3873b122bd37093a
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-04-14 10:42:16 +0530

    Commite message

--- BirdShooter/Shrihan.py
+++ BirdShooter/Shrihan.py
@@ -15,14 +15,14 @@ from pyglet.window import key, mouse
 TICKS_PER_SEC = 60
 
 # Size of sectors used to ease block loading.
-SECTOR_SIZE = 100
+SECTOR_SIZE = 10
 
 WALKING_SPEED = 15
 FLYING_SPEED = 1
 
 GRAVITY = 15
 
-MAX_JUMP_HEIGHT = 5 # About the height of a block.
+MAX_JUMP_HEIGHT = 2# About the height of a block.
 # To derive the formula for calculating jump speed, first solve
 #    v_t = v_0 + a * t
 # for the time at which you achieve maximum height, where a is the acceleration
