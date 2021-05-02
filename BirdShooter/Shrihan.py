429af90daca9e01bdc803621126cc0d33a6a8dad
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-05-02 21:10:28 +0530

    Commite message

--- BirdShooter/Shrihan.py
+++ BirdShooter/Shrihan.py
@@ -12,7 +12,7 @@ from pyglet.graphics import TextureGroup
 from pyglet.window import key, mouse
 
 
-TICKS_PER_SEC = 60
+TICKS_PER_SEC = 120
 
 # Size of sectors used to ease block loading.
 SECTOR_SIZE = 100
