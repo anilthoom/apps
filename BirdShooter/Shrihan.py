f4f001bfe45a94669dac8bbbc71d07cd624e587b
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-05-02 21:06:56 +0530

    Commite message

--- BirdShooter/Shrihan.py
+++ BirdShooter/Shrihan.py
@@ -18,11 +18,11 @@ TICKS_PER_SEC = 60
 SECTOR_SIZE = 100
 
 WALKING_SPEED = 3
-FLYING_SPEED = 120
+FLYING_SPEED = 0
 
-GRAVITY = 15
+GRAVITY = 30
 
-MAX_JUMP_HEIGHT = 2# About the height of a block.
+MAX_JUMP_HEIGHT = 1# About the height of a block.
 # To derive the formula for calculating jump speed, first solve
 #    v_t = v_0 + a * t
 # for the time at which you achieve maximum height, where a is the acceleration
@@ -30,7 +30,7 @@ MAX_JUMP_HEIGHT = 2# About the height of a block.
 #    t = - v_0 / a
 # Use t and the desired MAX_JUMP_HEIGHT to solve for v_0 (jump speed) in
 #    s = s_0 + v_0 * t + (a * t^2) / 2
-JUMP_SPEED = math.sqrt( 10*GRAVITY * MAX_JUMP_HEIGHT)
+JUMP_SPEED = math.sqrt( 2*GRAVITY * MAX_JUMP_HEIGHT)
 TERMINAL_VELOCITY = 2
 
 PLAYER_HEIGHT = 2
@@ -72,13 +72,13 @@ def tex_coords(top, bottom, side):
     result = []
     result.extend(top)
     result.extend(bottom)
-    result.extend(side *4)
+    result.extend(side *4 )
     return result
 
 
 TEXTURE_PATH = 'texture.png'
 
-GRASS = tex_coords((1, 0), (0, 1), (0, 0))
+GRASS = tex_coords((1, 1), (1, 1), (1, 0))
 SAND = tex_coords((1, 1), (1, 1), (1, 1))
 BRICK = tex_coords((2, 0), (2, 0), (2, 0))
 STONE = tex_coords((2, 1), (2, 1), (2, 1))
