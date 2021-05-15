ef6538588740842b949e691b283baf793f63f4dc
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-05-15 15:14:54 +0530

    Flashhhh

--- BirdShooter/Shrihan.py
+++ BirdShooter/Shrihan.py
@@ -22,7 +22,7 @@ FLYING_SPEED = 0
 
 GRAVITY = 15
 
-MAX_JUMP_HEIGHT = 1# About the height of a block.
+MAX_JUMP_HEIGHT = 15# About the height of a block.
 # To derive the formula for calculating jump speed, first solve
 #    v_t = v_0 + a * t
 # for the time at which you achieve maximum height, where a is the acceleration
@@ -30,7 +30,7 @@ MAX_JUMP_HEIGHT = 1# About the height of a block.
 #    t = - v_0 / a
 # Use t and the desired MAX_JUMP_HEIGHT to solve for v_0 (jump speed) in
 #    s = s_0 + v_0 * t + (a * t^2) / 2
-JUMP_SPEED = math.sqrt( 2*GRAVITY * MAX_JUMP_HEIGHT)
+JUMP_SPEED = math.sqrt( 1*GRAVITY * MAX_JUMP_HEIGHT)
 TERMINAL_VELOCITY = 2
 
 PLAYER_HEIGHT = 2
