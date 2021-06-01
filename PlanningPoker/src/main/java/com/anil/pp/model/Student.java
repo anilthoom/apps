f50a045d53e714cdbfdac73eccb3f6bdb5d6cb71
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-06-01 20:46:11 +0530

    PP App H2 Entity

---
+++ PlanningPoker/src/main/java/com/anil/pp/model/Student.java
@@ -0,0 +1,32 @@
+package com.anil.pp.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+//mark class as an Entity
+@Entity
+//defining class name as Table name
+@Table
+@Getter
+@Setter
+public class Student
+{
+    //mark id as primary key
+    @Id
+    //defining id as column name
+    @Column
+    private int id;
+    //defining name as column name
+    @Column
+    private String name;
+    //defining age as column name
+    @Column
+    private int age;
+    //defining email as column name
+    @Column
+    private String email;
+}
