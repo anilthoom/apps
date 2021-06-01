b4f9ad55a68e3d8fcff6e93d2eeae5dbd34fd75b
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-06-01 21:04:35 +0530

    Entity

--- PlanningPoker/src/main/java/com/anil/pp/model/Student.java
+++ PlanningPoker/src/main/java/com/anil/pp/model/Student.java
@@ -1,8 +1,5 @@
 package com.anil.pp.model;
 
-import lombok.Getter;
-import lombok.Setter;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -11,8 +8,6 @@ import javax.persistence.Table;
 @Entity
 //defining class name as Table name
 @Table
-@Getter
-@Setter
 public class Student
 {
     //mark id as primary key
@@ -29,4 +24,36 @@ public class Student
     //defining email as column name
     @Column
     private String email;
+    public int getId()
+    {
+        return id;
+    }
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+    public String getName()
+    {
+        return name;
+    }
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+    public int getAge()
+    {
+        return age;
+    }
+    public void setAge(int age)
+    {
+        this.age = age;
+    }
+    public String getEmail()
+    {
+        return email;
+    }
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
 }
