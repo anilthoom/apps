3ec1610f329cbd061096f709cba5b5138c531f47
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-06-01 21:20:29 +0530

    Maven Planning Poker App

---
+++ PlanningPokerApp/src/main/java/com/anil/pp/PlanningPokerAppApplication.java
@@ -0,0 +1,13 @@
+package com.anil.pp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanningPokerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanningPokerAppApplication.class, args);
+	}
+
+}
