c25bf0fe788e6d25c2d4c509119267c8df6819e2
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-06-02 15:29:11 +0530

    Duplicated project removal

--- PlanningPoker/src/main/java/com/anil/pp/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.anil.pp.repository;
-import com.anil.pp.model.Student;
-import org.springframework.data.repository.CrudRepository;
-
-public interface StudentRepository extends CrudRepository<Student, Integer>
-{
-}
