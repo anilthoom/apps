5af60c4cef4b3f2122a61136f300d679cdf7e465
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-06-06 16:40:44 +0530

    Refactoring

--- PlanningPoker/app.js
+++ PlanningPoker/app.js
@@ -3,7 +3,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-    res.sendFile('chat.html', {root: __dirname});
+    res.sendFile('app.html', {root: __dirname});
 });
 
 users = [];
