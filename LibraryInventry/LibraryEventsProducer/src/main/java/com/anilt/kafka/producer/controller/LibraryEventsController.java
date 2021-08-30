a322f622f8e22cf8fc00139f2a215592c49a765b
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-08-30 18:07:26 +0530

    Library Event Producer

--- LibraryInventry/LibraryEventsProducer/src/main/java/com/anilt/kafka/producer/controller/LibraryEventsController.java
+++ LibraryInventry/LibraryEventsProducer/src/main/java/com/anilt/kafka/producer/controller/LibraryEventsController.java
@@ -1,4 +1,18 @@
 package com.anilt.kafka.producer.controller;
 
+import com.anilt.kafka.producer.model.LibraryEvent;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class LibraryEventsController {
+
+    @PostMapping("/v1/libraryevent")
+    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent){
+        //invoke kafka producer
+        return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
+    }
 }
