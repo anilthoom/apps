b679c3f4982b8def451f5a7523fdaf8c658fdee0
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-08-31 14:39:14 +0530

    Kafka producer

--- LibraryInventry/LibraryEventProducer/src/main/java/com/anilt/kafka/producer/controller/LibraryEventsController.java
+++ LibraryInventry/LibraryEventProducer/src/main/java/com/anilt/kafka/producer/controller/LibraryEventsController.java
@@ -3,6 +3,7 @@ package com.anilt.kafka.producer.controller;
 import com.anilt.kafka.producer.model.LibraryEvent;
 import com.anilt.kafka.producer.producer.LibraryEventProducer;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,16 +11,22 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.ExecutionException;
+
 @RestController
+@Slf4j
 public class LibraryEventsController {
 
     @Autowired
     LibraryEventProducer libraryEventProducer;
 
     @PostMapping("/v1/libraryevent")
-    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException {
+    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException, ExecutionException, InterruptedException {
         //invoke kafka producer
-        libraryEventProducer.sendLibraryEvent(libraryEvent);
+        log.info("Before posting the event");
+        //libraryEventProducer.sendLibraryEvent(libraryEvent);
+        libraryEventProducer.sendLibraryEventSynchronous(libraryEvent);
+        log.info("After posting the event");
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
     }
 }
