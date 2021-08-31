cfefb70ffecb338e4b805d432ffe7af2ce85174e
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-08-31 13:25:15 +0530

    Kafka producer

---
+++ LibraryInventry/LibraryEventProducer/src/main/java/com/anilt/kafka/producer/producer/LibraryEventProducer.java
@@ -0,0 +1,25 @@
+package com.anilt.kafka.producer.producer;
+
+import com.anilt.kafka.producer.model.LibraryEvent;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class LibraryEventProducer {
+    @Autowired
+    KafkaTemplate<Integer, String> kafkaTemplate;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    public void sendLibraryEvent(LibraryEvent libraryEvent) throws JsonProcessingException {
+        Integer key = libraryEvent.getLibraryEventId();
+        String value = objectMapper.writeValueAsString(libraryEvent);
+        kafkaTemplate.sendDefault(key, value);
+    }
+}
