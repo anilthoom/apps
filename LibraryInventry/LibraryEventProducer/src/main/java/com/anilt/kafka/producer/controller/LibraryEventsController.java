0c2eb812c2ec880a3548aa7d189ec8668cac6598
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-09-03 16:33:32 +0530

    Kafka producer

--- LibraryInventry/LibraryEventProducer/src/main/java/com/anilt/kafka/producer/controller/LibraryEventsController.java
+++ LibraryInventry/LibraryEventProducer/src/main/java/com/anilt/kafka/producer/controller/LibraryEventsController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,5 +34,15 @@ public class LibraryEventsController {
         libraryEventProducer.sendLibraryEvent_Approach2(libraryEvent);
         log.info("After posting the event");
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
+    }
+
+    @PutMapping("/v1/libraryevent")
+    public ResponseEntity<?> putLibraryEvent(@RequestBody @Valid LibraryEvent libraryEvent) throws JsonProcessingException, ExecutionException, InterruptedException, TimeoutException {
+        if(libraryEvent.getLibraryEventId() == null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Library Event Id can't be null");
+        }
+        libraryEvent.setLibraryEventType(LibraryEventType.UPDATE);
+        libraryEventProducer.sendLibraryEvent_Approach2(libraryEvent);
+        return ResponseEntity.status(HttpStatus.OK).body(libraryEvent);
     }
 }
