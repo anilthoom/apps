114d0d0bef701b4e816fd0447eab0d697dd3f300
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-09-03 18:56:30 +0530

    Library Event Consumer Service

--- LibraryInventry/LibraryEventConsumer/src/main/java/com/anilt/kafka/consumer/consumer/LibraryEventsConsumer.java
+++ LibraryInventry/LibraryEventConsumer/src/main/java/com/anilt/kafka/consumer/consumer/LibraryEventsConsumer.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Component;
 @Component
 @Slf4j
 public class LibraryEventsConsumer {
-
     @KafkaListener(topics = {"library-events"})
     public void onMessage(ConsumerRecord<Integer, String> consumerRecord){
         log.info("Consumer record information : {}", consumerRecord);
