48d86d3aaadc3706e6529d3967e56f6359df70d2
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-09-05 11:59:24 +0530

    Speech recognition app

---
+++ SpeachRecognition/SpeechToText.py
@@ -0,0 +1,16 @@
+import speech_recognition as sr
+import pyttsx3
+
+# Initialize the recognizer
+r = sr.Recognizer()
+
+
+# Function to convert text to speech
+def SpeakText(command):
+    # Initialize the engine
+    engine = pyttsx3.init()
+    engine.say(command)
+    engine.runAndWait()
+
+
+# SpeakText("Hello Driver Ramya")
