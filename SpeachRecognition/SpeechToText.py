38323e47b3f45920f1351ac543de86bf383f605b
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-09-05 12:27:06 +0530

    Speech recognition app

--- SpeachRecognition/SpeechToText.py
+++ SpeachRecognition/SpeechToText.py
@@ -1,6 +1,7 @@
 import speech_recognition as sr
 import pyttsx3
 
+
 # Initialize the recognizer
 r = sr.Recognizer()
 
@@ -14,3 +15,19 @@ def SpeakText(command):
 
 
 # SpeakText("Hello Anil")
+with sr.Microphone() as source2:
+    # Wait for a second to let the recognizer
+    # adjust the energy threshold based on the surrounding noise level
+    print("Silence please, calibrating background noise")
+    r.adjust_for_ambient_noise(source2, duration=2)
+    print("Calibrated! Now speak...")
+
+    # Listen for the user input
+    audio2 = r.listen(source2)
+
+    # Using google to recognize the audio
+    MyText = r.recognize_google(audio2)
+    MyText = MyText.lower()
+
+    print(MyText)
+    SpeakText(MyText)
