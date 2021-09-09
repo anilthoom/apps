b37faeca1190cec19075c0754758342977bffec4
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-09-09 20:00:43 +0530

    Library Event Consumer Service

--- LibraryInventry/LibraryEventConsumer/src/main/java/com/anilt/kafka/consumer/entity/LibraryEvent.java
+++ LibraryInventry/LibraryEventConsumer/src/main/java/com/anilt/kafka/consumer/entity/LibraryEvent.java
@@ -5,6 +5,11 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -12,8 +17,13 @@ import lombok.NoArgsConstructor;
 @Builder
 public class LibraryEvent {
 
+    @Id
+    @GeneratedValue
     private Integer libraryEventId;
 
+    @Enumerated(EnumType.STRING)
     private LibraryEventType libraryEventType;
+
+
     private Book book;
 }
