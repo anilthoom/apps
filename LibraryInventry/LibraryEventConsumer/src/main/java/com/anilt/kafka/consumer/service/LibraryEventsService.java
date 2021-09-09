39b592e488a2ac1a93e77899cef87274a843c831
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-09-09 20:32:45 +0530

    Library Event Consumer Service

--- LibraryInventry/LibraryEventConsumer/src/main/java/com/anilt/kafka/consumer/service/LibraryEventsService.java
+++ LibraryInventry/LibraryEventConsumer/src/main/java/com/anilt/kafka/consumer/service/LibraryEventsService.java
@@ -1,6 +1,7 @@
 package com.anilt.kafka.consumer.service;
 
 import com.anilt.kafka.consumer.entity.LibraryEvent;
+import com.anilt.kafka.consumer.jpa.LibraryEventsRepository;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
@@ -15,8 +16,29 @@ public class LibraryEventsService {
     @Autowired
     ObjectMapper objectMapper;
 
+    @Autowired
+    private LibraryEventsRepository libraryEventsRepository;
+
     public void processLibraryEvent(ConsumerRecord<Integer, String> consumerRecord) throws JsonProcessingException {
         LibraryEvent libraryEvent = objectMapper.readValue(consumerRecord.value(), LibraryEvent.class);
         log.info("library event : {}", libraryEvent);
+        switch (libraryEvent.getLibraryEventType()){
+            case NEW:
+                //Save operation
+                save(libraryEvent);
+                break;
+            case UPDATE:
+                //Update
+                break;
+            default:
+                log.info("Invalid LibraryEventType");
+
+        }
+    }
+
+    private void save(LibraryEvent libraryEvent) {
+        libraryEvent.getBook().setLibraryEvent(libraryEvent);
+        libraryEventsRepository.save(libraryEvent);
+        log.info("Successfully persisted the library event : {}", libraryEvent);
     }
 }
