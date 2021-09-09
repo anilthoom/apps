b84f95da98df1b2c78d6fa716202fda69ff82caa
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-09-09 20:23:55 +0530

    Library Event Consumer Service

--- LibraryInventry/LibraryEventConsumer/src/main/java/com/anilt/kafka/consumer/service/LibraryEventsService.java
+++ LibraryInventry/LibraryEventConsumer/src/main/java/com/anilt/kafka/consumer/service/LibraryEventsService.java
@@ -1,4 +1,21 @@
 package com.anilt.kafka.consumer.service;
 
+import com.anilt.kafka.consumer.entity.LibraryEvent;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
 public class LibraryEventsService {
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    public void processLibraryEvent(ConsumerRecord<Integer, String> consumerRecord) throws JsonProcessingException {
+        LibraryEvent libraryEvent = objectMapper.readValue(consumerRecord.value(), LibraryEvent.class);
+    }
 }
