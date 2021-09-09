285f06e74ae782dfccef96012bdd0bc27a7cde69
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-09-09 19:59:40 +0530

    Library Event Consumer Service

---
+++ LibraryInventry/LibraryEventConsumer/src/main/java/com/anilt/kafka/consumer/entity/LibraryEvent.java
@@ -0,0 +1,19 @@
+package com.anilt.kafka.consumer.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class LibraryEvent {
+
+    private Integer libraryEventId;
+
+    private LibraryEventType libraryEventType;
+    private Book book;
+}
