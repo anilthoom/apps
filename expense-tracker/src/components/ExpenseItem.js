9d4d86c2dad57a0c73a99de86b6e15a2feb03e02
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-09-22 17:00:37 +0530

    CSS

--- expense-tracker/src/components/ExpenseItem.js
+++ expense-tracker/src/components/ExpenseItem.js
@@ -4,7 +4,7 @@ function ExpenseItem() {
         <div className="expense-item">
             <div>22 Sep 2021</div>
             <div className="expense-item__description">
-                <h1>Car Insurance</h1>
+                <h2>Car Insurance</h2>
                 <div className="expense-item__price">$123</div>
             </div>
         </div>
