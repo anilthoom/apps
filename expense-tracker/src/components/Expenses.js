f026507401c32e6368fe734a631cfe303569c578
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-09-23 21:02:31 +0530

    Styles

--- expense-tracker/src/components/Expenses.js
+++ expense-tracker/src/components/Expenses.js
@@ -1,13 +1,15 @@
 import ExpenseItem from "./ExpenseItem";
-
+import "./Expenses.css"
 
 function Expenses(props) {
     const items = props.items;
     debugger;
     return (
-        <div>
+        <div className="expenses">
             <ExpenseItem title={items[0].title} date={items[0].date} amount={items[0].amount}></ExpenseItem>
             <ExpenseItem title={items[1].title} date={items[1].date} amount={items[1].amount}></ExpenseItem>
+            <ExpenseItem title={items[2].title} date={items[2].date} amount={items[2].amount}></ExpenseItem>
+            <ExpenseItem title={items[3].title} date={items[3].date} amount={items[3].amount}></ExpenseItem>
         </div>
     );
 }
