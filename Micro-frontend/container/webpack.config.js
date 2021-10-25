078bd597f97da7f814b0c1bc7af748951a0041a6
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-10-25 16:19:12 +0530

    Module federation

--- Micro-frontend/container/webpack.config.js
+++ Micro-frontend/container/webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const packageJson = require('./package.json')
 
 module.exports = {
     mode: 'development',
@@ -7,6 +9,13 @@ module.exports = {
         port: 3000,
     },
     plugins: [
+        new ModuleFederationPlugin({
+            name: 'container', 
+            remotes: {
+                products: 'products@http://localhost:8000/remoteEntry.js', // importing products file from port 8000
+                cart: 'cart@http://localhost:8001/remoteEntry.js', // importing cart file from port 8001
+            },
+        }),
         new HTMLWebpackPlugin({
             template: path.resolve(__dirname, 'public', 'index.html'),
         })
