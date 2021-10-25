f93f5d8c28216ba8dabe0cd80e1f919605fd827b
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-10-25 16:14:38 +0530

    webpack config

--- Micro-frontend/cart/webpack.config.js
+++ Micro-frontend/cart/webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const packageJson = require('./package.json')
 
 module.exports = {
     mode: 'development',
@@ -7,6 +9,14 @@ module.exports = {
         port: 8001,
     },
     plugins: [
+        new ModuleFederationPlugin({
+            name: 'cart', // name of the item being exposed (required in container)
+            filename: 'remoteEntry.js', // name of the file
+            exposes: {
+                './Cart': './src/bootstrap' // actual file being exposed
+            },
+            shared: packageJson.dependencies, // to remove duplicate external modules loading in the container
+        }),
         new HTMLWebpackPlugin({
             template: path.resolve(__dirname, 'public', 'index.html'),
         })
