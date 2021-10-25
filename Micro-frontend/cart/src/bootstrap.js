adc666cc249d50b99df2bc26826fa59545532466
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-10-25 16:19:22 +0530

    bootstrap

---
+++ Micro-frontend/cart/src/bootstrap.js
@@ -0,0 +1,15 @@
+import faker from 'faker'
+
+const mount = (element) => {
+  // generating fake data
+  element.innerHTML = `
+  <p>You have ${faker.datatype.number(10)} items in your cart</p>
+`
+}
+
+const mountPt = document.querySelector('#dev-cart')
+if (process.env.NODE_ENV === 'development' && mountPt) {
+  mount(document.querySelector('#dev-cart'))
+}
+
+export { mount } // sharing generic mount function
