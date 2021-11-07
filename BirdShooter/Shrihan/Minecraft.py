2255b299910d1b7b2503b33f6b6146d28f3208da
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-11-07 21:10:21 +0530

    Sector_size

--- BirdShooter/Shrihan/Minecraft.py
+++ BirdShooter/Shrihan/Minecraft.py
@@ -14,7 +14,7 @@ from pyglet.window import key, mouse
 TICKS_PER_SEC = 60
 
 # Size of sectors used to ease block loading.
-SECTOR_SIZE = 16
+SECTOR_SIZE = 1
 
 WALKING_SPEED = 15
 FLYING_SPEED = 15
